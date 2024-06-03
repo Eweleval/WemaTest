e10f176df280e205952b91d752a9517d1f24174a
Wellz <valeweleokenyi@gmail.com>
2024-06-03 14:32:44 +0100

    Initial Commit

---
+++ WemaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WemaTest
+//
+//  Created by Wellz Val on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
